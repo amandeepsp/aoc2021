24d499884f877cca0059fafee611faac11fc4d79
Amandeep Singh <amandeepspdhr@gmail.com>
2021-12-24 19:18:28 +0530

    Day 07 Fix

--- src/aoc/day07.clj
+++ src/aoc/day07.clj
@@ -8,10 +8,12 @@
 (defn median [coll]
   (let [sorted-coll (sort coll)
         len (count coll)
-        mid (int (/ len 2))]
+        mid (int (/ len 2))
+        mid-elem (nth sorted-coll mid)
+        middec-elem (nth sorted-coll (dec mid))]
     (if (odd? len)
-      (nth sorted-coll mid)
-      [(nth sorted-coll (dec mid)) (nth sorted-coll mid)])))
+      mid-elem
+      (int (/ (+ mid-elem middec-elem) 2)))))
 
 (defn abs-cost [final-pos pos]
   (abs (- pos final-pos)))
@@ -20,10 +22,8 @@
   (apply + (map (partial cost-fn final-pos) positions)))
 
 ; Part 1
-(let [pos-median (median positions)]
-  (if (coll? pos-median)
-    (apply min (map (partial calc-cost positions abs-cost) pos-median))
-    (calc-cost positions abs-cost pos-median)))
+(let [final-pos (median positions)]
+  (calc-cost positions abs-cost final-pos))
 
 (defn mean [coll]
   (/ (apply + coll) (count coll)))
